94b0bd1d7e9cf0f3e4e303e79668ae48dbbcb533
sayali singh <sayalibaghel6@gmail.com>
2025-05-04 21:01:27 +0530

    new

--- empy-variable-delcare.py
+++ empy-variable-delcare.py
@@ -54,3 +54,61 @@ print(x)
 
 x= set(d)
 print(x)
+
+n = input("entre a value")
+
+# x = int()
+# print(x)
+
+# x = float()
+# print(x)
+
+# x= complex()
+# print(x)
+
+# x= str(n)
+# print(x)
+
+
+x= list(n)
+print(x)
+
+
+x= tuple(n)
+print(x)
+
+
+
+x= str(n)
+print(x)
+
+
+# typecasting not use in dictionary
+# x= dict(n)
+# print(x)
+
+d ={'hii':'say' ,'age':27}
+
+
+# string give all value while other give key only
+x = str(d)
+print(x)
+
+x = frozenset(d)
+print(x)
+
+
+x= list(d)
+print(x)
+
+
+x= tuple(d)
+print(x)
+
+
+
+x= set(d)
+print(x)
+
+x= set(d)
+print(x)
