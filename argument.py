8c7beb055f7ef30da0e291d44e95089cdc450b18
sayali singh <sayalibaghel6@gmail.com>
2025-05-16 20:01:41 +0530

    new

---
+++ argument.py
@@ -0,0 +1,59 @@
+# keyword argument - relation between argument and parameter
+def fibp(x,y,z):
+    print("value of x =",x)
+
+p =10
+q=20 
+r= 30 
+# fibp()   
+# # fibp(p)
+# fibp(p,q)
+fibp(p,r,q)
+# fibp(p,q,r,s)
+
+
+# default argument
+def fibp(x=0,y=0,z=0):
+    print("value of x =",x)
+    print("value of y =",y)
+
+p =10
+q=20 
+r= 30 
+fibp()   
+fibp(p)
+fibp(p,q)
+# fibp(p,r,q,s)
+
+
+# variable length positionl argument - it hold data in form of tuple (* args)
+def fibp(* n):
+    print(n)
+    print(type(n))
+
+# p =10
+# q=20 
+# r= 30 
+# fibp()   
+# fibp(p)
+# fibp(p,q)
+fibp(3,3,4,)
+
+
+def fibo(* args):
+    sum=0
+    for i in args:
+        sum=sum+i
+    return sum    
+x= fibo(2,4,6,8) 
+print(x)   
+
+def fibo(* args):
+    sum=0
+    for i in args:
+        for j in i:
+         sum=sum+j
+    return sum    
+p=(3,4,56,)
+x= fibo(p) 
+print(x)   
