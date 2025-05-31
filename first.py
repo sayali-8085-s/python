171732fcc7b98f723798fd81b12966046ddaa392
sayali singh <sayalibaghel6@gmail.com>
2025-05-31 21:11:37 +0530

    new

---
+++ first.py
@@ -0,0 +1,11 @@
+x = 10
+v=10
+
+def add (p,q):
+    return(p+q)
+
+
+class student:
+    def detail(self):
+        print("hello from class")
+
---
+++ module__/sec.py
@@ -0,0 +1,31 @@
+# it is faster then other
+
+from first import x
+print(x)
+
+
+
+from first import add
+h= add(10,20)
+print(h)
+
+
+from first import student
+
+
+x =student()
+x.detail()
+
+
+
+# alliyasin - to make nick name
+from first import student as std
+x=std()
+x.detail()
+
+
+
+
+# or -it take time
+# import first
+# print(first.x)
