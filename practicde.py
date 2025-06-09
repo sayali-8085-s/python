0a5df2ab553728ae978cca61d3601f08782c0e91
sayali singh <sayalibaghel6@gmail.com>
2025-06-09 21:02:27 +0530

    new

---
+++ practicde.py
@@ -0,0 +1,56 @@
+# # n natural even no.
+# # n = int(input("Enter a number: "))
+# for i in range(2, 2*n + 1,2):
+#    if n%2 ==0:
+#     if i<2*n:
+#      print(i,end=',')
+#     else:
+#      print(i)
+# else:
+#   if i<2*n-1:
+#    print(i,end=",")
+#   else:
+#    print(i)
+# # n natural odd no.
+# # 
+
+# # even natural no.
+# # n=int(input("enter a no"))
+# for i in range(1,n+1):
+#   if i<n:
+#     print(2*i,end=',')
+#   else:
+#    print(2*i)
+
+
+
+
+
+
+# for upto n odd natural no.
+# n=int(input("enter a no"))
+# for i in range(1,n+1,2):
+#     if(n%2!=0):
+#       if i<n:
+#         print(i,end=',')
+#       else:
+#        print(i)
+#     else:
+#       if(i<n-1):
+#        print(i,end=',')
+#       else:
+#        print(i)
+
+
+n=int(input("enter a no"))
+for i in range(1,n+1,2):
+    if(n%2!=0):
+      if i<n:
+        print(i,end=',')
+      else:
+       print(i)
+    else:
+      if(i<n-1):
+       print(i,end=',')
+      else:
+       print(i)       
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,75 +1,12 @@
-
-# reduce() - 
-
-from functools import reduce
-l =[1,8,5,6,7]
-
-def maxv(x,y):
-    if x>y:
-       return x
-    else:
-       return y
-
-       
-x=reduce(maxv,l)
-print(x)
-
-
-
-from functools import reduce
-l =[1,8,5,6,7]
-
-def minv(x,y):
-    if x<y:
-       return x
-    else:
-       return y
-
-       
-x=reduce(minv,l)
-print(x)
-
-
-from functools import reduce
-l =[1,8,5,6,7]
-
-def sum(x,y):
-       return x+y
-    
-       
-       
-x=reduce(sum,l)
-print(x)
-
-
-# quet
-
-
-from functools import reduce
-l =[1,23,4]
-
-def add5(n):
-    return n+1
-x=map(add5,l)
-
-p=tuple(x)
-  
-def add(p,m):
-    return p+m
-
-y =reduce(add,p)
-print(y)
-
-
-# or 
-
-# x = reduce()
-
-
-# 4)decorator -spcl function whicch change internal behviour of function without change code
-# it take function as parameter and return function
-
-# python tutor -imp for debugging
-
-
-def outer_fun(f):
+n=int(input("enter a no"))
+# for i in range(1,n+1,2):
+#     if(n%2!=0):
+#       if i<n:
+#         print(i,end=',')
+#       else:
+#        print(i)
+#     else:
+#       if(i<n-1):
+#        print(i,end=',')
+#       else:
+#        print(i)
